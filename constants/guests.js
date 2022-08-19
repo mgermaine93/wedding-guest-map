ea755b6476bbfedf7b269e7869585bc4263a45e9
mgermaine93 <mgermaine93@gmail.com>
2022-08-19 17:44:04 -0400

    Working on adding markers to map.

--- constants/guests.js
+++ constants/guests.js
@@ -1,3 +1,11 @@
-let guests = {
-    // TBD
+const fs = require('fs');
+
+fs.onload = function (event) {
+    console.log(event.target.result);
 }
+
+fs.readAsText("wedding-geodata.csv")
+
+// let guests = {
+//     // TBD
+// }
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-let maxBounds = [
-    [5.499550, -167.276413], //Southwest
-    [83.162102, -52.233040]  //Northeast
-];
+// let maxBounds = [
+//     [5.499550, -167.276413], //Southwest
+//     [83.162102, -52.233040]  //Northeast
+// ];
 
+
+// The Leaflet map object, centered to Wooster, OH
 let map = L.map("map").setView([40.8051, -81.9351], 13);
 
+// References the OpenStreetMap tile layer (the actual map)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-map.setMaxBounds(maxBounds);
-map.fitBounds(maxBounds);
+// First Pres
+let marker = L.marker([40.80592067927781, -81.93569411534408]).addTo(map);
+// map.setMaxBounds(maxBounds);
+// map.fitBounds(maxBounds);
