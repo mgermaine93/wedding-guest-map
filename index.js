78c77366c6b67f62e950fff47b9fdc37ce476abd
mgermaine93 <mgermaine93@gmail.com>
2022-08-25 17:39:30 -0400

    Adding minor changes to test loading of map.

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// let maxBounds = [
-//     [5.499550, -167.276413], //Southwest
-//     [83.162102, -52.233040]  //Northeast
-// ];
-
+import json from './guests.json';
 
 // The Leaflet map object, centered to Wooster, OH
 let map = L.map("map").setView([40.8051, -81.9351], 13);
@@ -14,8 +10,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // First Pres
-let marker = L.marker([40.80592067927781, -81.93569411534408]).addTo(map);
+let church = L.marker([40.80592067927781, -81.93569411534408]).addTo(map);
 
+for (let i = 0; i < json.length; i++) {
+    marker = new L.marker(
+        [
+            json['latitude'],
+            json['longitude']
+        ]
+    ).bindPopup(json['name']).addTo(map);
+}
 
 
 // map.setMaxBounds(maxBounds);
