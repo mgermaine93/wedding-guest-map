c0a532b301d80402cb0d0a7ee5e2e561d57c73fd
mgermaine93 <mgermaine93@gmail.com>
2022-10-10 16:29:21 -0400

    Heatmap/Chloropleth diagram layer is working now.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+// Defines the church marker and makes it its own layer so that it shows when the page initially loads.
 let church = L.marker([40.805270, -81.935620]).bindPopup('This is where we got married!');
 let churchGroup = L.layerGroup([church]);
 
-// groups all of the map points together in their respective groups, using the functions below
+// usStates = L.geoJson(usStates, {
+//     style: style,
+//     onEachFeature: onEachFeature
+// })
+
+// Uses the helper functions (defined elsewhere) to create the different groups of guests based on criteria.
+let stateOutlines = L.geoJson(usStatesData, {
+  style: style,
+  onEachFeature: onEachFeature
+})
 let couple = L.geoJSON(guestsJson, {
   filter: coupleFilter,
   onEachFeature: onEachFeature
@@ -75,30 +85,30 @@ let vendors = L.geoJSON(guestsJson, {
   onEachFeature: onEachFeature
 })
 
-// loads in the specific Open Street Map map/"tile" to use
+// Loads in the specific Open Street Map map/"tile" to use
 let osm = L.tileLayer(
     'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap',
 });
 
-// set to center onto the geographic center of the USA
+// Set the center onto the geographic center of the USA
 let map = L.map('map', {
   center: [39.8283, -98.5795],
   zoom: 4.5,
-  layers: [osm, churchGroup]
+  layers: [osm, churchGroup] // These load when the page is initially loaded
 });
-// .setView([39.8283, -98.5795], 5);
 
-// defines the base open street map view that we see upon page load
+// Defines the base open street map view that we see upon page load
 let baseMaps = {
-  "OpenStreetMap": osm
+  "OpenStreetMap": osm,
 }
 
-// defines the different layers we can select in the control
+// Defines the different layers we can select in the control
 let overlayMaps = {
-  // different groups of points go here
+  // Different groups of points go here
   "Church": churchGroup,
+  "State Outlines": stateOutlines,
   "Couple": couple,
   "Invitees": invited,
   "Attendees": attended,
@@ -119,30 +129,8 @@ let overlayMaps = {
   "Vendors": vendors
 }
 
+// Basically sets the control menu in the upper right of the map
 let layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-// let couplePoints = L.geoJson(guestsJson, {filter: coupleFilter}).addTo(map);
-// let couplePoints = L.layerGroup(guestsJson, {filter: coupleFilter})
-
-
-// // trying to create a textbox over the map
-// L.Control.textbox = L.Control.extend({
-
-// 		onAdd: function(map) {
-			
-//       let text = L.DomUtil.create('div');
-//       text.id = "info_text";
-//       text.innerHTML = "<strong>Wedding Map 2022</strong>"
-//       return text;
-
-// 		},
-
-// 		onRemove: function(map) {
-// 			// Nothing to do here
-// 		}
-// 	});
-
-// 	L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
-// 	L.control.textbox({ position: 'topleft' }).addTo(map);
 
 // HELPER FUNCTIONS THAT PARSE GUESTS INTO DIFFERENT GROUPS BASED ON CRITERIA
 function onEachFeature(feature, layer) {
@@ -150,8 +138,14 @@ function onEachFeature(feature, layer) {
   if (feature.properties && feature.properties.popupContent) {
     layer.bindPopup(feature.properties.popupContent);
   }
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    // click: zoomToFeature,
+  })
 }
 
+// The Couple
 function coupleFilter(feature) {
   if (feature.properties.is_groom || feature.properties.is_bride) {
     return true;
@@ -278,3 +272,78 @@ function isVendorFilter(feature) {
     return true;
   }
 }
+
+// Get colors based on population density
+function getColor(dataPoint) {
+  return dataPoint > 100 ? '#7f0000' :
+         dataPoint > 50 ? '#990000' :
+         dataPoint > 20  ? '#d7301f' :
+         dataPoint > 10  ? '#ef6548' :
+         dataPoint > 5  ? '#fc8d59' :
+         dataPoint > 2   ? '#fdbb84' :
+         dataPoint > 1   ? '#fdd49e' :
+         dataPoint == 1   ? '#fee8c8' :
+                           '#fff7ec'; 
+}
+
+// Styles the colors based on the density determined above
+function style(feature) {
+  return {
+    fillColor: getColor(feature.properties.density),
+    weight: 2,
+    opacity: 1,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.6
+  };
+}
+
+function highlightFeature(e) {
+    let layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',  // dark grey
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+  // Important to reset the highlights of the states data, NOT the guests data!
+  stateOutlines.resetStyle(e.target);
+}
+
+// function zoomToFeature(e) {
+//     map.fitBounds(e.target.getBounds());
+// }
+
+// geojson = L.geojson(guestsJson);
+
+// let couplePoints = L.geoJson(guestsJson, {filter: coupleFilter}).addTo(map);
+// let couplePoints = L.layerGroup(guestsJson, {filter: coupleFilter})
+
+
+// // trying to create a textbox over the map
+// L.Control.textbox = L.Control.extend({
+
+// 		onAdd: function(map) {
+			
+//       let text = L.DomUtil.create('div');
+//       text.id = "info_text";
+//       text.innerHTML = "<strong>Wedding Map 2022</strong>"
+//       return text;
+
+// 		},
+
+// 		onRemove: function(map) {
+// 			// Nothing to do here
+// 		}
+// 	});
+
+// 	L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+// 	L.control.textbox({ position: 'topleft' }).addTo(map);
